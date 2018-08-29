7b4206f05e5a0e969672738348bd271460d4c3d1
Ignacio R Espino <irespino@capozios.com>
2018-08-29 02:33:16 +0000

    Done.

--- my_each.rb
+++ my_each.rb
@@ -1,3 +1,9 @@
-def my_each # put argument(s) here
-  # code here
+def my_each(arr)
+  counter = 0
+  while counter < arr.length  do
+    yield (arr[counter])
+    counter += 1
+  end
+  
+  return arr
 end
